e221960a6fd4e06b240c1e32f2e38e82123e754c
Nickolas Cunningham <nickolascunningham@gmail.c.com>
2021-10-01 14:57:15 -0400

    final commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,51 @@
-$( document ).ready( onReady );
+const passengersDom = $("#passengersOut")
+const form = $("form")
 
-function onReady(){
-    console.log( 'JQ' );
-} // end onReady
+
+function getPassengers() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:5000/passenger",
+    })
+        .then(passengers => {
+            passengers.forEach(passenger => {
+                const markUp = `<li>${passenger.firstName} ${passenger.lastName}</li>`
+                passengersDom.append(markUp)
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+
+form.submit((event) => {
+    event.preventDefault();
+    const formData = {
+        firstName: $("#firstNameIn").val(),
+        lastName: $("#lastNameIn").val()
+    }
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        datatype: "json",
+        data: JSON.stringify(formData),
+        url: "http://localhost:5000/passenger" })
+
+        .then(res => {
+            passengersDom.empty();
+           getPassengers();
+        })
+        .catch(error => console.log(error))
+
+        $("#firstNameIn").val("")
+        $("#lastNameIn").val("")
+})
+
+
+
+
+
+
+getPassengers()
--- server/server.js
+++ server/server.js
@@ -2,25 +2,38 @@
 const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
+const cors = require("cors")
+
 // uses
 app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use(cors())
+
+app.use(express.json())
+
 // globals
 const port = 5000;
 // spin up server
+
+let passengers = [ { firstName: 'Commander', lastName: 'Wisdom' } ,
+{ firstName: 'Teacher', lastName: 'Anthony' } 
+]
+
+
 app.listen( port, ()=>{
     console.log( 'server up on:', port );
 })
 // routes
 
-/****** template routes *******
-app.get( '/NAME', ( req, res )=>{
-    console.log( 'NAME GET hit' );
-    res.send( THINGSTOSEND )
+app.get( '/passenger', (req, res)=>{
+    res.send(passengers);
 })
 
-app.post( '/NAME', ( req, res )=>{
+
+app.post( '/passenger', ( req, res )=>{
     console.log( 'NAME POST hit:', req.body );
-    res.sendStatus( 200 ) // 200 = generic OK, 201 = created
+    passengers.push(req.body)
+    res.send(passengers) // 200 = generic OK, 201 = created
 })
- */
+ 
+ 
